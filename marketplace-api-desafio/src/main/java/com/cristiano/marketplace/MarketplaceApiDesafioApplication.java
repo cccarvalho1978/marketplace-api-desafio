d36f2eefc21b365a588af07c9a5077dafdcc6180
Cristiano <cristiano.carvalho@gmail.com>
2020-11-11 20:25:59 -0300

    Commitando início do projeto.

---
+++ marketplace-api-desafio/src/main/java/com/cristiano/marketplace/MarketplaceApiDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.cristiano.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceApiDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceApiDesafioApplication.class, args);
+	}
+
+}
---
+++ marketplace-api-desafio/src/test/java/com/cristiano/marketplace/MarketplaceApiDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristiano.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApiDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
